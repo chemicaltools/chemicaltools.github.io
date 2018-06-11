867a82b0d080dc0620cd5dfd37e1d2c742402123
njzjz <njzjz@qq.com>
2018-06-11 21:48:39 +0800

    update url

--- android/index.php
+++
@@ -1,3 +0,0 @@
-<html>
-<script type="text/javascript">window.location.href='https://f.zgchemicals.mobi/com.njzjz.chemicaltools_2.02.apk';</script>
-</html>
--- ios/index.php
+++
@@ -1,3 +0,0 @@
-<html>
-<script type="text/javascript">window.location.href='https://f.zgchemicals.mobi/chemicaltools.ipa';</script>
-</html>
